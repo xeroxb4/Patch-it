d1d05722f5055c90fea3227e130b0c98452a1b8a
xeroxb4 <xeroxb4@gmail.com>
2024-11-12 14:07:25 +0000

    patch 1

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+
+// Create Express App
+const app = express();
+
+
+
+// use middleware
+app.use
+
+
+
+// Listen for incoming requests
+app.listen(7080, () => {
+    console.log('App listening on port 7080');
+});
